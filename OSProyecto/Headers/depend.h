b02aefbef88967827a034a6223f974f8422b652f
TheBirb <mmjontxu@gmail.com>
2021-10-22 22:45:51 +0200

    V1

--- OSProyecto/Headers/depend.h
+++ OSProyecto/Headers/depend.h
@@ -10,9 +10,12 @@
 struct PCB{
     int pid;
     int ttl;
+    int prioridad;
     struct PCB *next;
 };
 typedef struct PCB PCB;
+//LA RUNQUEUE TENEMOS QUE PONER CUANTOS QUEREMOS MAX EN TODA LA RUNQUEUE
+
 struct pcq{
     int max;
     int count;
--- OSProyecto/Source/Maquina.c
+++ OSProyecto/Source/Maquina.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../Headers/depend.h"
-
+#include <semaphore.h>
 
 
 //hay que tener un PCB en 0, para que cuando un hilo este vacío
@@ -17,28 +17,82 @@ pcq *pcqxCPU;
 pthread_mutex_t lock;
 pthread_cond_t condP;
 pthread_cond_t condS;
-pthread_cond_t condD;
 pthread_cond_t sigT;
+pthread_mutex_t lock;
+sem_t semQ;
 int times[] = {2,3,4};
 int cpus, cores, hilos;
 struct PCB pcb0;
-struct pcq processQ;
+struct pcq newProcesses;
+int PID=1;
+int nhejec=0;
+//------METODOS DE LA COLA---------
+void pushQ(pcq *cola, PCB *pcb){
+    if(cola->count==0){
+        cola -> inicio = pcb;
+        cola->final = pcb;
+    }else{
+        cola->final->next = pcb;
+    }
+    cola->final = pcb;
+    cola->count++;
+}
+PCB *pullQ(pcq *cola){
+    PCB *pcb = cola->inicio;
+    if(cola->count==1){
+        cola->inicio=NULL;
+        cola->final=NULL;
+    }else{
+        cola->inicio= cola->inicio->next;
+    }
+    cola->count--;
+    return(pcb);
+}
+PCB *atomicQueue(int type, pcq *cola, PCB *pcb){
+    sem_wait(&semQ);
+    pthread_mutex_lock(&lock);
+    PCB *npcb;
+    if(type==1){
+        pushQ(cola, pcb);
+    }else{
+        npcb = pullQ(cola);
+    }
+    pthread_mutex_unlock(&lock);
+    sem_post(&semQ);
+    return(npcb);
+}
+
 void *clk(){
+    int i,j,k;
     while(1){
+        printf("%d\n",newProcesses.count);
         cf++;
         sleep(1);
         printf("Clock %d\n",cf);
         pthread_cond_signal(&sigT);
+        for(i=0; i<cpus; i++){
+            for(j=0; j<cores; j++){
+                for(k=0; k<cores; k++){
+                    if(CpuList[i].core[j].hilos[k].MyProc->pid!=0){
+                        if(CpuList[i].core[j].hilos[k].MyProc->ttl==0){
+                            nhejec--;
+                            CpuList[i].core[j].hilos[k].MyProc->pid=0;
+                        }else{
+                            CpuList[i].core[j].hilos[k].MyProc->ttl--;
+                        }
+                    }
+                }
+            }
+        }
+        printf("%d\n",nhejec);
     }
 }
 void *timer(int index){
     //index=0 => processGen
-    //index=1 => sheduler
-    //index=2 => dispatcher
+    //index=1 => scheduler
     int tiempo= times[index];
     int d1=0;
     int d2=0;
-    int d3=0;
     if(index==0){
         while(1){
             pthread_cond_wait(&sigT, &lock);
@@ -48,7 +102,7 @@ void *timer(int index){
                 d1=0;
             }
         }
-    }else if(index==1){
+    }else{
         while(1){
             pthread_cond_wait(&sigT, &lock);
             d2++;
@@ -57,58 +111,47 @@ void *timer(int index){
                 d2=0;
             }
         }
-    }else{
-        while(1){
-            pthread_cond_wait(&sigT, &lock);
-            d3++;
-            if(d3==tiempo){
-                pthread_cond_signal(&condD);
-                d3=0;
-            }
-        }
     }
 
 
 }
 void *processGen(){
-    int ttl;
     while(1){
         pthread_cond_wait(&condP, &lock);
-        printf("TrabajandoGenProc, %d\n",cf);
-
+        if(newProcesses.max > newProcesses.count){
+            PCB *npcb = malloc(sizeof(struct PCB));
+            npcb->pid = PID;
+            PID++;
+            npcb->ttl=rand() % 2;
+            npcb->prioridad= rand() % 99;
+            atomicQueue(1, &newProcesses, npcb);
+        }
     }
 }
 void *scheduler(){
-    while(1){
-        pthread_cond_wait(&condS, &lock);
-        printf("TrabajandoScheduler, %d\n", cf);
-        sleep(2);
-    }
-}
-void *dispatcher(){
     int i,j,k;
     while(1){
-        pthread_cond_wait(&condD, &lock);
-        //miramos cola de ready
-        printf("TrabajandoDispatcher, %d\n", cf);
-        for(i=0; i<cpus; i++){
-            for(j=0; j<cores; j++){
-                for(k=0; k<cores; k++){
-                   if(CpuList[i].core[j].hilos[k].MyProc->pid==0){
-                       //vacio procesamos lista de ready
-                   }else{
-                       if(CpuList[i].core[j].hilos[k].MyProc->ttl==0){
-                           CpuList[i].core[j].hilos[k].MyProc->pid=0;
-                       }else{
-                           CpuList[i].core[j].hilos[k].MyProc->ttl--;
-                       }
-                   }
+        pthread_cond_wait(&condS, &lock);
+        if(newProcesses.count>0){
+            for(i=0; i<cpus; i++){
+                for(j=0; j<cores; j++){
+                    for(k=0; k<cores; k++){
+                        if(CpuList[i].core[j].hilos[k].MyProc->pid==0){
+                            if(newProcesses.count!=0){
+                                PCB *process = atomicQueue(2,&newProcesses,NULL);
+                                if (process != NULL) {
+                                    CpuList[i].core[j].hilos[k].MyProc=process;
+                                    nhejec++;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-
 }
+
 int main(){
     int i,j,k;
     pcb0.pid=0;
@@ -135,20 +178,22 @@ int main(){
             }
         }
     }
+    //COLA DE NEW
+    newProcesses.max=100;
 
-    pthread_t tid[6];
+    //THREADS
+    pthread_t tid[5];
     pthread_create(&tid[0], NULL, clk, NULL);
     pthread_create(&tid[1], NULL,processGen, NULL);
     pthread_create(&tid[2], NULL,scheduler, NULL);
-    pthread_create(&tid[3], NULL,dispatcher, NULL);
-    pthread_create(&tid[4], NULL, timer(0), NULL);
-    pthread_create(&tid[5], NULL, timer(1), NULL);
+    pthread_create(&tid[3], NULL, timer(0), NULL);
+    pthread_create(&tid[4], NULL, timer(1), NULL);
+    //ESPERAMOS A QUE TERMINEN
     pthread_join(tid[0],NULL);
     pthread_join(tid[1],NULL);
     pthread_join(tid[2],NULL);
     pthread_join(tid[3],NULL);
     pthread_join(tid[4],NULL);
-    pthread_join(tid[5],NULL);
     return 0;
 }
 

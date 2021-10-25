bab5c0a1fb0db8132999899e2a18df7885e5cde8
TheBirb <60443461+TheBirb@users.noreply.github.com>
2021-10-25 16:03:59 +0200

    V0.3

--- OSProyecto/Headers/depend.h
+++ OSProyecto/Headers/depend.h
@@ -10,10 +10,14 @@
 struct PCB{
     int pid;
     int ttl;
+    int prioridad;
     struct PCB *next;
 };
 typedef struct PCB PCB;
+//LA RUNQUEUE TENEMOS QUE PONER CUANTOS QUEREMOS MAX EN TODA LA RUNQUEUE
+
 struct pcq{
+    int max;
     int count;
     PCB *inicio;
     PCB *final;

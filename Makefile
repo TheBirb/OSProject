a63b5728d926d12a92da500c0e739aa2bd140a62
TheBirb <mmjontxu@gmail.com>
2021-11-21 16:47:33 +0100

    V1.1

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ HEADERS= Headers/depend.h
 Default: Maquina
 
 Maquina.o: Source/Maquina.c $(HEADERS)
-	gcc -c Source/Maquina.c -o Maquina.o
+	gcc -c Source/Maquina.c -o Maquina.o -lpthread
 Program: Maquina.o
-	gcc Maquina.o -o Maquina
+	gcc Maquina.o -o Maquina 
 Clean:
 	-rm -f Maquina.o
 	-rm -f Maquina
--- Source/Maquina.c
+++ Source/Maquina.c
@@ -16,16 +16,14 @@ int cpus, cores, hilos;
 pthread_mutex_t lock;
 pthread_cond_t sigT;
 pthread_cond_t broadcast;
-pthread_mutex_t lock;
 pthread_mutex_t clkm;
+pthread_mutex_t locka;
 int flag=0;
 
 //estructura de control de timers,
 //El timer[0] es el numero de clocks que espera el process generator
 //El timer[1] es el numero de clocks que espera el scheduler
-int times[] = {3,4};
-//Estructura procesos
-struct PCB pcb0;
+int times[] = {2,5};
 //Estructuras RTC
 struct RTC realTimeClass;
 int PID=1;
@@ -42,9 +40,9 @@ int done=0;
  * @return
  */
 void *pushQ(PCB *pcb, int prioridad){
-    if( realTimeClass.bitmap[prioridad]==0){
-        realTimeClass.pQ[prioridad]-> inicio = pcb;
-        realTimeClass.pQ[prioridad]->final = pcb;
+    printf("prioridad del push :  %d \n", prioridad);
+    if(realTimeClass.pQ[prioridad]->count==0){
+        realTimeClass.pQ[prioridad]->inicio = pcb;
         realTimeClass.bitmap[prioridad]=1;
     }else{
         realTimeClass.pQ[prioridad]->final->next = pcb;
@@ -52,6 +50,7 @@ void *pushQ(PCB *pcb, int prioridad){
     realTimeClass.pQ[prioridad]->final = pcb;
     realTimeClass.pQ[prioridad]->count++;
     realTimeClass.count++;
+
     return 0;
 }
 
@@ -89,35 +88,35 @@ void *clk() {
         while(done<2){
             pthread_cond_wait(&sigT, &clkm);
         }
-        LOCK:
-        while(flag==1){}
-        if(flag==0){
-            flag=1;
-        }else{
-            goto LOCK;
-        }
+        pthread_mutex_lock( &locka );
         for (i = 0; i < cpus; i++) {
             for (j = 0; j < cores; j++) {
                 for (k = 0; k < hilos; k++) {
                     if (CpuList[i].core[j].hilos[k].MyProc->pid != 0) {
                         if (CpuList[i].core[j].hilos[k].MyProc->ttl == 0) {
-                            CpuList[i].core[j].hilos[k].MyProc->pid = 0;
+                            PCB *pcb=(PCB*)malloc(sizeof(struct PCB));
+                            pcb->pid=0;
+                            CpuList[i].core[j].hilos[k].MyProc=pcb;
                             nhejec--;
                         } else {
                             if(CpuList[i].core[j].hilos[k].MyProc->quantumRestante==0){
                                 pushQ(CpuList[i].core[j].hilos[k].MyProc, CpuList[i].core[j].hilos[k].MyProc->prioridad);
-                                CpuList[i].core[j].hilos[k].MyProc->pid = 0;
+                                PCB *pcb=(PCB*)malloc(sizeof(struct PCB));
+                                pcb->pid=0;
+                                CpuList[i].core[j].hilos[k].MyProc=pcb;
+
                                 nhejec--;
                             }else{
                                 CpuList[i].core[j].hilos[k].MyProc->quantumRestante--;
+                                CpuList[i].core[j].hilos[k].MyProc->ttl--;
                             }
-                            CpuList[i].core[j].hilos[k].MyProc->ttl--;
+
                         }
                     }
                 }
             }
         }
-        flag=0;
+        pthread_mutex_unlock( &locka );
         done=0;
         sleep(1);
         printf("N procesos: %d\n", nhejec);
@@ -131,13 +130,18 @@ void *clk() {
  * Generador de procesos
  */
 void processGen(){
+    printf("soy PG\n");
     PCB *npcb = (PCB*)malloc(sizeof(struct PCB));
     npcb->pid = PID;
     PID++;
     npcb->ttl=rand() % 300;
     int prio =rand() % 99;
+    if (prio==0){
+        prio=1;
+    }
     npcb->prioridad= prio;
     npcb->quantum=10;
+    printf("prio %d \n", prio);
     if(realTimeClass.pQ[npcb->prioridad]->count<MAX){
         pushQ( npcb, npcb->prioridad);
     }else{
@@ -158,15 +162,9 @@ void *timer1(){
         done++;
         d1++;
         if(d1==tiempo){
-            LOCK:
-            while(flag==1){}
-            if(flag==0){
-                flag=1;
-            }else{
-                goto LOCK;
-            }
+            pthread_mutex_lock( &locka );
             processGen();
-            flag=0;
+            pthread_mutex_unlock( &locka );
             d1=0;
         }
         pthread_cond_signal(&sigT);
@@ -179,26 +177,27 @@ void *timer1(){
  * Scheduler con política RTC
  * @return
  */
-void *scheduler(){
+void *scheduler(){printf("soy SC\n");
     int i,j,k,l;
     int index=0;
+    printf("hola-1\n");
     if(realTimeClass.count>0){
+        printf("hola\n");
         for(i=0; i<cpus; i++){
             for(j=0; j<cores; j++){
-                for(k=0; k<cores; k++){
+                for(k=0; k<hilos; k++){
                     if(CpuList[i].core[j].hilos[k].MyProc->pid==0){
-                        PCB *process=NULL;
+
                         for (l = index; l < 100; ++l) {
                             if (realTimeClass.bitmap[l] == 1) {
-                                process = pullQ(l);
+                                printf("Meto[%d]\n", l);
+                               PCB *process = pullQ(l);
+                                process->quantumRestante=process->quantum;
+                                CpuList[i].core[j].hilos[k].MyProc=process;
                                 nhejec++;
                                 index=l;
                                 break;
                             }
-                        }
-                        if (process != NULL) {
-                            process->quantumRestante=process->quantum;
-                            CpuList[i].core[j].hilos[k].MyProc=process;
                         }
                     }
                 }
@@ -221,15 +220,9 @@ void *timer2(){
         done++;
         d2++;
         if(d2==tiempo){
-            LOCK:
-            while(flag==1){}
-            if(flag==0){
-                flag=1;
-            }else{
-                goto LOCK;
-            }
+            pthread_mutex_lock( &locka );
             scheduler();
-            flag=0;
+            pthread_mutex_unlock( &locka );
             d2=0;
         }
         pthread_cond_signal(&sigT);
@@ -240,7 +233,6 @@ void *timer2(){
 
 int main() {
     int i, j, k;
-    pcb0.pid = 0;
     printf("Cuantos CPU\n");
     scanf("%d", &cpus);
     printf("Cuantos Cores por CPU: \n");
@@ -258,8 +250,10 @@ int main() {
             CpuList[i].core[j] = core;
             CpuList[i].core[j].hilos = (hilo *) malloc(hilos * sizeof(hilo));
             for (k = 0; k < hilos; k++) {
+                PCB *pcb=(PCB*)malloc(sizeof(struct PCB));
+                pcb->pid=0;
                 struct hilo hilo;
-                hilo.MyProc = &pcb0;
+                hilo.MyProc = pcb;
                 CpuList[i].core[j].hilos[k] = hilo;
             }
         }
@@ -278,7 +272,7 @@ int main() {
     pthread_cond_init(&sigT, NULL);
     pthread_cond_init(&broadcast, NULL);
     pthread_mutex_init(&clkm,NULL);
-    pthread_mutex_init(&lock,NULL);
+    pthread_mutex_init(&locka,NULL);
     //INICIALIZACIÓN DE THREADS
     pthread_t tid[5];
     pthread_create(&tid[0], NULL, clk, NULL);
